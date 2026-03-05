417d2f6edf8b2cd805b705d7b30be57d309ccf14
Jelagat2640 <sugutasumpta@gmail.com>
2026-03-06 02:49:09 +0300

    final code

--- testing/test_palindrome.py
+++ testing/test_palindrome.py
@@ -1,34 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytest
-
-
-def longest_palindromic_substring(s):
-
-    if not isinstance(s, str):
-        raise TypeError("Input must be a string")
-
-    if len(s) == 0:
-        return ""
-
-    start = 0
-    end = 0
-
-    def expand(left, right):
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        return left + 1, right - 1
-
-    for i in range(len(s)):
-        l1, r1 = expand(i, i)
-        l2, r2 = expand(i, i + 1)
-
-        if r1 - l1 > end - start:
-            start, end = l1, r1
-
-        if r2 - l2 > end - start:
-            start, end = l2, r2
-
-    return s[start:end + 1]
+from lib.palindrome import longest_palindromic_substring
 
 
 @pytest.mark.parametrize("input_string, expected", [
